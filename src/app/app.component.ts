50162d33d027470d7a17a0b1941f009f96f49ce7
Marbelis Poleo <poleomoncada@gmail.com>
2022-10-18 20:00:57 -0300

    appListaDeEmpleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { empleado } from './empleado.models';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appListaDeEmpleado';
+  titulo = 'Lista de empleado';
+   empleados:empleado[]=[
+
+    new empleado("Juan","Diaz","Presidente",7500),
+    new empleado("Ana","Martin","Direccion",5500),
+    new empleado("Maria","Fdez","Jfa seccion",3500),
+    new empleado("Laura","Lopez","Administrativo",2500),
+
+  ];
+
+  agregarEmpleado(){
+
+    let miEmpleado=new  empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+
+  }
+
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+
 }
+
